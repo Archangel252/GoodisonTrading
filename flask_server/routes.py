401cf050f9f8671b7bf687c3892dd1df3c818bb6
Archangel252 <adixon91302@gmail.com>
2024-07-31 22:58:49 -0400

    controller init

--- flask_server/routes.py
+++ flask_server/routes.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, current_app as app
 import threading
-from flask_server.bot.bot_main import run_bot
 import os 
 
 
@@ -9,4 +8,4 @@ def root():
     return render_template('index.html')
 
 def start_bot():
-    run_bot()
+    pass
